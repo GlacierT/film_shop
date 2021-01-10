8a58e405afff02dd7247109baf9ff6817ee5d3bd
T_Tanya <tikhonovskyat@gmail.com>
2021-01-11 01:00:19 +0300

    Updates

---
+++ precache-manifest.47eb302420537cd355e4d57e788e68b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "897885e32f3bf50b6a8a",
+    "url": "/film_shop/static/css/main.22f2d131.chunk.css"
+  },
+  {
+    "revision": "897885e32f3bf50b6a8a",
+    "url": "/film_shop/static/js/main.01a9394d.chunk.js"
+  },
+  {
+    "revision": "f8da1da4081b0414e045",
+    "url": "/film_shop/static/js/runtime~main.a5c22ae9.js"
+  },
+  {
+    "revision": "3fdd8ce9b4a1036e2b96",
+    "url": "/film_shop/static/css/2.0dea9590.chunk.css"
+  },
+  {
+    "revision": "3fdd8ce9b4a1036e2b96",
+    "url": "/film_shop/static/js/2.bd882405.chunk.js"
+  },
+  {
+    "revision": "1e69f67f6d30bd387b9dc5a6624f652a",
+    "url": "/film_shop/static/media/logo.1e69f67f.svg"
+  },
+  {
+    "revision": "a4e1e681e47240c9f9c50cb08dd3e99e",
+    "url": "/film_shop/static/media/header.a4e1e681.png"
+  },
+  {
+    "revision": "2c62727acce77b3f786a81c796045f80",
+    "url": "/film_shop/index.html"
+  }
+];
